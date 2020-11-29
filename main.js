6c99e2ce6e63d06850c6bd27b3542861a7a7d16d
Lissa <raylisswhile@gmail.com>
2020-11-29 15:53:01 +0500

    Программа работает,но интерфейса не имеет

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-var fs = require('fs');
+const fs = require('fs');
+const util = require('util');
+
+const readdir = util.promisify(fs.readdir);
+const mkdir = util.promisify(fs.mkdir);
+const stat = util.promisify(fs.stat);
 
 //var myPath = process.argv[2];
 //console.log(process.argv);
 var prev="";
 var tek="";
-var path='F:\\фото\\выгрузка 2019\\Видео';
+var path='F:\\фото\\выгрузка 2019\\выгрузка';
 //__dirname
 
 function move(oldPath, newPath, callback) {
@@ -36,7 +41,7 @@ function move(oldPath, newPath, callback) {
     }
 }
 
-
+/*
 fs.readdir(path, function(err, items) {
 
   for (var i = 0; i < items.length; i++) {
@@ -67,3 +72,37 @@ fs.readdir(path, function(err, items) {
 
   }
 });
+*/
+
+(async ()=>{
+
+  const items = await readdir(path)
+  for (let i = 0; i < items.length; i++) {
+    const file = path + '/' + items[i];
+    const fileName = '' + items[i];
+    console.log("Start: " + file);
+    console.log("Fun: " + fileName);
+    const stats = await stat(file);
+    tek = new Date(stats["mtime"]);
+    tek = tek.getDate() + '.' + (tek.getMonth() + 1) + '.' + tek.getFullYear();
+    //  console.log(tek);
+    console.log(tek + '      ' + prev);
+    if (tek !== prev) {
+      console.log('да');
+      try {
+        await mkdir(path + '/' + tek);
+      } catch (err){ move(path + '/' + fileName, path + '/' + tek + '/' + fileName, ()=>{});};
+    //  await mkdir(path + '/' + tek);
+      console.log('делаю папку ' + path + '/' + tek);
+      move(path + '/' + fileName, path + '/' + tek + '/' + fileName, ()=>{});
+      console.log('помещаю фото ' + path + '/' + tek + '/' + fileName);
+      //  fs.writeFileSync(path+'/'+tek+'/'+fileName,data);
+      prev = tek;
+    } else {
+      console.log('else ' + fileName);
+      move(path + '/' + fileName, path + '/' + tek + '/' + fileName, ()=>{});
+    }
+    //else fs.writeFileSync(path+'/'+tek+'/'+fileName );
+  }
+
+})();
