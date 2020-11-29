db425b28f48eef6100080240935d415cb012afa1
Lissa <raylisswhile@gmail.com>
2020-11-29 15:34:30 +0500

    cddcd

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 //console.log(process.argv);
 var prev="";
 var tek="";
-var path='E:test';
+var path='F:\\фото\\выгрузка 2019\\Видео';
 //__dirname
 
 function move(oldPath, newPath, callback) {
@@ -39,31 +39,31 @@ function move(oldPath, newPath, callback) {
 
 fs.readdir(path, function(err, items) {
 
-    for (var i=0; i<items.length; i++) {
-              var file = path + '/' + items[i];
-              var fileName=''+items[i];
-              //  console.log("Start: " + file);
-             console.log("Fun: " + fileName);
-             fs.stat(file, function(err, stats) {
-             tek=new Date (stats["mtime"]);
-             tek=tek.getDate()+'.'+(tek.getMonth()+1)+'.'+ tek.getFullYear();
-                  //  console.log(tek);
-                 console.log(tek+'      '+ prev);
-                   if(tek!==prev){
-                       console.log('да');
-                                 //console.log(path+'/'+tek+'/'+fileName);
-                         fs.mkdirSync(path+'/'+tek);
-                                 console.log('делаю папку '+ path+'/'+tek);
-                         move(path+'/'+fileName,path+'/'+tek+'/'+fileName,cb=>(err));
-                                 console.log('помещаю фото '+ path+'/'+tek+'/'+fileName);
-                                 //  fs.writeFileSync(path+'/'+tek+'/'+fileName,data);
-                        prev=tek;
-                   }
-                   else {
-                                 console.log('else '+fileName );
-                         move(path + '/' + fileName, path + '/' + tek + '/' + fileName, cb => (err));
-                   }
-                                 //else fs.writeFileSync(path+'/'+tek+'/'+fileName );
-         });
+  for (var i = 0; i < items.length; i++) {
+    var file = path + '/' + items[i];
+    var fileName = '' + items[i];
+    console.log("Start: " + file);
+    console.log("Fun: " + fileName);
+    const stats = fs.statSync(file);
+    tek = new Date(stats["mtime"]);
+    tek = tek.getDate() + '.' + (tek.getMonth() + 1) + '.' + tek.getFullYear();
+    //  console.log(tek);
+    console.log(tek + '      ' + prev);
+    if (tek !== prev) {
+      console.log('да');
+      if (readdir)
+      fs.mkdirSync(path + '/' + tek);
+      console.log('делаю папку ' + path + '/' + tek);
+      move(path + '/' + fileName, path + '/' + tek + '/' + fileName, cb => (err));
+      console.log('помещаю фото ' + path + '/' + tek + '/' + fileName);
+      //  fs.writeFileSync(path+'/'+tek+'/'+fileName,data);
+      prev = tek;
+    } else {
+      console.log('else ' + fileName);
+      move(path + '/' + fileName, path + '/' + tek + '/' + fileName, cb => (err));
     }
+    //else fs.writeFileSync(path+'/'+tek+'/'+fileName );
+
+
+  }
 });
